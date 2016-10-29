7d82c5e152de6372d5e46e51f23cae40e76b5633
Prateek Sharma <prateek0sharma@gmail.com>
2016-10-29 16:34:02 +0530

    latest changes & addition of ang-mom directory

--- Bondi_PS_Cluster/source_jet.c
+++ Bondi_PS_Cluster/source_jet.c
@@ -20,6 +20,7 @@ void Src_jet (const Data *d, Grid *grid)
   double psi_jet, mdot_acc, darea, norm_jet;
   double sendarray[1], recvarray[1];
   double RJET, SIGRJ, THJ, d3;
+  double TinK, tau_dep=200.0*CONST_Myr;
 
     x1=grid[IDIR].x; x2=grid[JDIR].x; x3=grid[KDIR].x;
     dx1=grid[IDIR].dx; dx2=grid[JDIR].dx; dx3=grid[KDIR].dx;
@@ -126,24 +127,6 @@ void Src_jet (const Data *d, Grid *grid)
     MPI_Comm_free(&comm2d);
     MPI_Comm_free(&comm1d);
     #endif
-
-/*    KDOM_LOOP(k){
-    JDOM_LOOP(j){
-    mdot_acc += x1[IBEG]*x1[IBEG]*sin(x2[j])*dx2[j]*dx3[k] \
-              *d->Vc[RHO][k][j][IBEG]*d->Vc[VX1][k][j][IBEG];
-    }}
-    #ifdef PARALLEL
-    }
-    sendarray[0] = mdot_acc;
-    MPI_Allreduce (sendarray, recvarray, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);  
-    mdot_acc = recvarray[0];
-    #endif
-    
-    if (mdot_acc < 0.0){
-      mdot_jet = -g_inputParam[EFF]*mdot_acc; //this is actually epsilon*Mdot
-    }else{
-      mdot_jet = 0.0;
-    } */
 /* *********************************************************************** */
     DOM_LOOP(k,j,i){
        psi_jet = 0.25*(2.0 + tanh((THJ - x2[j])/g_inputParam[SIGTH]) \
@@ -152,5 +135,12 @@ void Src_jet (const Data *d, Grid *grid)
        d->Vc[RHO][k][j][i] += g_dt*norm_jet*mdot_jet*psi_jet;
        d->Vc[VX1][k][j][i] += g_dt*norm_jet*mdot_jet*g_inputParam[VJET]*psi_jet\
        /(d->Vc[RHO][k][j][i]*UNIT_VELOCITY);
+
+       d->Vc[TRC][k][j][i] += g_dt*norm_jet*mdot_jet*psi_jet/d->Vc[RHO][k][j][i];
+       TinK   = d->Vc[PRS][k][j][i]*CONST_mu*CONST_mp/(d->Vc[RHO][k][j][i]*CONST_kB);
+       if (TinK < 5.e4 && d->Vc[RHO][k][j][i] > 1.e-24){
+         msub += x1[i]*x1[i]*sin(x2[j])*dx2[j]*dx3[k]*dx1[i]*d->Vc[RHO][k][j][i]*( 1.0- exp(-g_dt/tau_dep) );
+         d->Vc[RHO][k][j][i] = d->Vc[RHO][k][j][i]*exp(-g_dt/tau_dep);
+       }
     }
 }
