5fd889354bd48c9642d57c1e66296128e9778984
prateek sharma <prateek0sharma@gmail.com>
2017-04-20 17:15:48 +0530

    changes mage in app_global.h

--- Bondi_PS_Cluster/app_globals.h
+++ Bondi_PS_Cluster/app_globals.h
@@ -1,2 +1,5 @@
 double g_largePressure = 1.e2, madd, eadd, msub, mdot_jet;
 double g_d_outer, g_p_outer;
+
+double g_smallDensity  = 1.e-9; /**< Small value for density fix. */
+double g_smallPressure = 1.e-9; /**< Small value for pressure fix. */
